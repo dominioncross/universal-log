99fa0d9eb1897953ef87b5d6f84dad3fae600ee0
Ben Petro <benpetro@gmail.com>
2017-03-30 01:30:54 +0000

    log cleanup

--- app/models/universal_log/concerns/logged.rb
+++ app/models/universal_log/concerns/logged.rb
@@ -10,17 +10,33 @@ module UniversalLog::Concerns
       end
       
     end
+    def self.html_log_format_value(v)
+      if v.class == TrueClass
+        return 'Yes'
+      elsif v.class == FalseClass
+        return 'No'
+      elsif v.class == NilClass
+        return 'N/A'
+      elsif v.class == Array
+        return v.join(', ')
+      elsif v.class == Time
+        return v.strftime('%b %d, %Y')
+      elsif v.class == Date
+        return v.strftime('%b %d, %Y - %I:%M%p')
+      else 
+        return v
+      end
+    end
     
     module ClassMethods
       def html_log_format(doc, attributes)
         h = []
         puts attributes
         attributes.each do |att|
-          h.push("<div class=\"log-item\"><span class=\"log-label\">#{att[1]}:</span> <span class=\"log-value\">#{doc[att[0]]}</span></div>")
+          h.push("<div class=\"log-item\"><span class=\"log-label\">#{att[1]}:</span> <span class=\"log-value\">#{Logged.html_log_format_value(doc[att[0]])}</span></div>")
         end
         h.join('')
       end
-      
     end
   end
 end
